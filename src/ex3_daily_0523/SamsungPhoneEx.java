81f58632643a68351d663a9903143573f664410a
user-k-y <kjcom0529@hanmail.net>
2022-05-23 19:05:29 +0900

    0523_daily_recording

---
+++ src/ex3_daily_0523/SamsungPhoneEx.java
@@ -0,0 +1,47 @@
+package ex3_daily_0523;
+
+public class SamsungPhoneEx {
+	
+	public static void main(String[] args) {
+	
+		SamsungPhone sam = new SamsungPhone();
+		
+		sam.setCompany("ªÔº∫");
+		sam.setModel("∞∂∑∞Ω√8");
+		sam.setRelease(2018);
+		sam.setColor("∞À¡§");
+		
+		System.out.println(sam);
+		
+//		sam.setCompany("LG");
+//		sam.setModel("G6");
+//		sam.setRelease(2017);
+//		sam.setColor("ª°∞≠");
+//		
+//		System.out.println(sam.toString());
+		
+		
+		SamsungPhone sam1 = new SamsungPhone();
+		
+		sam1.setCompany("LG");
+		sam1.setModel("G6");
+		sam1.setRelease(2017);
+		sam1.setColor("ª°∞≠");
+		
+		
+		System.out.println(sam1.toString());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}//main
+	
+
+}
